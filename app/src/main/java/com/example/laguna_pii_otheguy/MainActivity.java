4922015bea266317c8f20e6d51eee596a4ff3d20
motheguy <motheguy@ensc.fr>
2021-02-17 23:02:58 +0100

    first

---
+++ app/src/main/java/com/example/laguna_pii_otheguy/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.laguna_pii_otheguy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
